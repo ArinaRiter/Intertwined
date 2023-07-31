e348134d38c9519b12d81265b79c941f37c691c2
ArinaRiter <117931793+ArinaRiter@users.noreply.github.com>
2023-07-31 16:54:26 +0300

    state

---
+++ Assets/Scripts/CharacterJumpState.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterJumpState :   CharacterBaseState
+{
+    public override void OnCollisionStateEnter(CharacterStateManager character, Collision collision)
+    {
+
+    }
+    public override void EnterState(CharacterStateManager character)
+    {
+
+    }
+    public override void UpdateState(CharacterStateManager character)
+    {
+
+    }
+}
