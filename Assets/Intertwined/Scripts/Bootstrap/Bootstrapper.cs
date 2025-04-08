e566ecdf7bf48dd31d50229b7acb70c39f500c08
ArinaRiter <117931793+ArinaRiter@users.noreply.github.com>
2025-04-08 17:48:21 +0300

    added bootstrapper (#4)

---
+++ Assets/Intertwined/Scripts/Bootstrap/Bootstrapper.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+
+public static class Bootstrapper 
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    public static void Bootstrap()
+    {
+        Object.DontDestroyOnLoad(Object.Instantiate(Resources.Load("Systems")));
+        
+        LoadGameScene();
+    }
+
+    private static void LoadGameScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Main");
+    }
+}
