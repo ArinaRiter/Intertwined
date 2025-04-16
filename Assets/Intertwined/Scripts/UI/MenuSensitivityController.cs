65888fb55f93c4992e50d3d9ca51cd7031e1723c
ArinaRiter <117931793+ArinaRiter@users.noreply.github.com>
2025-04-16 18:56:32 +0300

    added game menu

--- Assets/Intertwined/Scripts/UI/MenuSensitivityController.cs
+++ Assets/Intertwined/Scripts/UI/MenuSensitivityController.cs
@@ -8,10 +8,11 @@ using UnityEngine.UI;
 public class MenuSensitivityController : MonoBehaviour
 {
     [SerializeField] private Slider sensitivitySlider;
-    [SerializeField] private float defaultSensitivity;
+    [SerializeField] private float defaultSensitivity = 3f;
     [SerializeField] private TextMeshProUGUI sensitivityText;
+    [SerializeField] private GameObject settingsPanel;
     private const string SENSITIVITY_KEY = "MouseSensitivity";
-    private float initialSensitivity;
+    private float _initialSensitivity;
 
     private void Start()
     {
@@ -20,10 +21,10 @@ public class MenuSensitivityController : MonoBehaviour
 
     private void InitializeSlider()
     {
-        initialSensitivity = PlayerPrefs.GetFloat(SENSITIVITY_KEY, defaultSensitivity);
-        sensitivitySlider.value = initialSensitivity;
+        _initialSensitivity = PlayerPrefs.GetFloat(SENSITIVITY_KEY, defaultSensitivity);
+        sensitivitySlider.value = _initialSensitivity;
         sensitivitySlider.onValueChanged.AddListener(HandleSensitivityChanged);
-        sensitivityText.text = Math.Round(initialSensitivity, 1).ToString(CultureInfo.CurrentCulture);
+        sensitivityText.text = Math.Round(_initialSensitivity, 1).ToString(CultureInfo.CurrentCulture);
     }
     
     
@@ -32,19 +33,28 @@ public class MenuSensitivityController : MonoBehaviour
         sensitivityText.text = Math.Round(value, 1).ToString(CultureInfo.CurrentCulture);
     }
 
+    private void Update()
+    {
+        if (!settingsPanel.activeSelf)
+        {
+            NormalizeSensitivity();
+        }
+    }
+
     public void NormalizeSensitivity()
     {
-        float savedSensitivity = PlayerPrefs.GetFloat(SENSITIVITY_KEY, defaultSensitivity);
+        var savedSensitivity = PlayerPrefs.GetFloat(SENSITIVITY_KEY, defaultSensitivity);
         sensitivitySlider.value = savedSensitivity;
         sensitivityText.text = Math.Round(savedSensitivity, 1).ToString(CultureInfo.CurrentCulture);
     }
 
     public void SaveSensitivityChanged()
     {
-        initialSensitivity = sensitivitySlider.value;
-        PlayerPrefs.SetFloat(SENSITIVITY_KEY, initialSensitivity);
+        _initialSensitivity = sensitivitySlider.value;
+        PlayerPrefs.SetFloat(SENSITIVITY_KEY, _initialSensitivity);
         PlayerPrefs.Save();
-        Debug.Log($"Sensitivity saved: {initialSensitivity}");
+        Debug.Log($"Sensitivity saved: {_initialSensitivity}");
+        
     }
 
     private void OnDestroy()
